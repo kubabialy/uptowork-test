52dfab306babad4fb35a0237bf359046177e8c87
Kuba Biały <kuba.bialy.92@icloud.com>
2018-04-13 17:57:24 +0200

    Credentials adjustment

--- resources/views/main.php
+++ resources/views/main.php
@@ -36,7 +36,7 @@
 
     <footer class="mastfoot mt-auto">
         <div class="inner">
-            <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+            <p>Cover template based on <a href="https://twitter.com/mdo">@mdo</a> work.</p>
         </div>
     </footer>
 </div>
